1267ae585e627522057ee8bb6fe8656c26e9e7b5
Ziping Liu <ziping@utexas.edu>
2019-06-20 14:54:10 -0500

    second commit

--- FFTLib.h
+++ FFTLib.h
@@ -20,7 +20,7 @@ cmplx* FFT(cmplx* samples, size_t size);
 cmplx** FFT2(cmplx** samples, size_t num_rows, size_t num_cols);
 
 
-
+// Requires preallocation of buffers size of the signal _size
 void FFT_preallocation_expected(cmplx* samples, size_t size, cmplx* buff_0, cmplx* buff_1,
                                 cmplx* output_buffer_even, cmplx* output_buffer_odd, bool output_type, bool flip);
 
